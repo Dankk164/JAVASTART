403ca176cdc70e2c238a5f680b5780ff2e82568f
daniel <danielbbiton2001@gmail.com>
2023-10-12 22:43:26 +0300

    homework

---
+++ src/lesson/homework1.java
@@ -0,0 +1,14 @@
+package lesson;
+
+public class homework1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a=7;
+        b=9;
+        a=b;
+        b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
