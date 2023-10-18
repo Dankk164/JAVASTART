adff78a5090f21fb1ced85f0c479f0fb23b2f8c2
daniel <danielbbiton2001@gmail.com>
2023-10-18 13:14:15 +0300

    boolean

---
+++ src/homeString1.java
@@ -0,0 +1,12 @@
+public class homeString1 {
+    public static void main(String[] args) {
+        String string = "Testing, is my favourite job";
+        int werb1= "Testing".length();
+        int werb2=  "is".length();
+        boolean boo;
+        if (werb1>werb2) boo = true;
+        else boo = false;
+        System.out.println(boo);
+
+    }
+}
