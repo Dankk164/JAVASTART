df3bec9c73ebb8ccf593247f3ad858e860745cf8
daniel <danielbbiton2001@gmail.com>
2023-10-23 15:07:01 +0300

    дз 3 до 4 задачі

--- src/scanner.java
+++ src/scanner.java
@@ -1,16 +1,51 @@
 import java.util.Scanner;
 
 public class scanner {
-    public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        System.out.println("Я чудово тестую, що ще потрібно?");
-        String string1= scanner.next("Я");
-        System.out.println(string1);
-        String string2=scanner.next();
-        System.out.println("чудово");
+    //public static void main(String[] args) {
+        //Scanner scanner =  new Scanner(System.in);
+        //System.out.println("Я чудово тестую, що ще потрібно?");
+       // String string1= scanner.next("Я");
+       // System.out.println(string1);
+       // String string2=scanner.next();
+       // System.out.println("чудово");
+    //public static void main(String[] args) {
+        //За допомогою тернарного оператора потрібно отримати різницю між двома числами, введеними з клавіатури, і завжди віднімати менше від більшого. Виведіть цю різницю на консоль.
+      //  Scanner scanner=new Scanner(System.in);
+        //System.out.println("Введіть число");
+        //int a = scanner.nextInt();
+        //int b = scanner.nextInt();
+        //System.out.println((a>b)?(a-b):(b-a));
+    //public static void main(String[] args) {
+       // Scanner scanner=new Scanner(System.in);
+       // System.out.println("Введіть число");
+       // int a= scanner.nextInt();
+        //if(a==1) {
+           //System.out.println("Понеділок");
 
-    }
+
+      //  }else if(a==2) {
+
+            //System.out.println("Вівторок");
+       // }else {
+            //System.out.println("Краше сьогодні була б субота");
+
+           // }
+   // }
 
 
 
-}
+
+       }
+
+   //}
+
+
+
+
+
+    //}
+   //}
+
+
+
+//}
